e6123048a6e202ee5c0a403684d8645d8175a3f5
saltyjared <jaredpaulgue@gmail.com>
2024-11-03 20:48:43 -0800

    Implement account creation and store registered accounts in dictionary

--- banking_system_impl.py
+++ banking_system_impl.py
@@ -4,7 +4,12 @@ from banking_system import BankingSystem
 class BankingSystemImpl(BankingSystem):
 
     def __init__(self):
-        # TODO: implement
-        pass
+        # Encapsulate registered accounts as a hashmap/dictionary with account IDs as keys and balances as 
+        self.accounts = {}
 
     # TODO: implement interface methods here
+    def create_account(self, timestamp, account_id):
+        if account_id in self.accounts:
+            return False
+        self.accounts[account_id] = 0
+        return True
