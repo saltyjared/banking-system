994f1e7a47f9d2a681757f857b16cfe42a225d2a
rachana.baskar <rachana_baskar@berkeley.edu>
2024-11-10 12:14:42 -0800

    deposit

--- banking_system_impl.py
+++ banking_system_impl.py
@@ -7,4 +7,26 @@ class BankingSystemImpl(BankingSystem):
         # TODO: implement
         pass
 
+    def deposit(self, timestamp: int, account_id: str, amount: int) -> int | None:
+        """
+        Should deposit the given `amount` of money to the specified
+        account `account_id`.
+        Returns the balance of the account after the operation has
+        been processed.
+        If the specified account doesn't exist, should return
+        `None`.
+        """
+        # default implementation
+              
+        if account_id not in self.accounts:
+          return None
+        else: 
+          self.accounts[account_id] += amount # Add to balances in account id
+          return self.accounts[account_id]
+
+
+
+
     # TODO: implement interface methods here
+
+
